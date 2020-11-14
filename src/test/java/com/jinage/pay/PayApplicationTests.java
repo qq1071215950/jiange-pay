7449db7aca0c1d990aa0d4fb41d7c110a0c93bd6
jiange <5128535+jjiange@user.noreply.gitee.com>
2020-11-14 13:45:54 +0800

    支付系统的项目初始化，初始化数据源

---
+++ src/test/java/com/jinage/pay/PayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jinage.pay;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
